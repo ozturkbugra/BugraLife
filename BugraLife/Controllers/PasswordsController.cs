0d976797252b455a89d9cce6875a87f5ec1d3842
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-12-14 21:32:10 +0300

    Web Site Şifrelerim Kısmı CRUD yapıldı

---
+++ BugraLife/Controllers/PasswordsController.cs
@@ -0,0 +1,72 @@
+﻿using BugraLife.DBContext;
+using BugraLife.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class PasswordsController : Controller
+{
+    private readonly BugraLifeDBContext _context;
+
+    public PasswordsController(BugraLifeDBContext context)
+    {
+        _context = context;
+    }
+
+    // 1. LİSTELEME
+    public async Task<IActionResult> Index()
+    {
+        var passwords = await _context.WebSitePasswords
+                                      .Include(w => w.WebSite)
+                                      .ToListAsync();
+
+        ViewBag.WebSites = await _context.WebSites.ToListAsync();
+
+        return View(passwords);
+    }
+
+    // 2. EKLEME (POST - AJAX)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(WebSitePassword password)
+    {
+        if (ModelState.IsValid)
+        {
+            password.created_at = DateTime.Now; 
+            _context.WebSitePasswords.Add(password);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true, message = "Şifre başarıyla eklendi!" });
+        }
+        return Json(new { success = false, message = "Lütfen tüm alanları doldurun." });
+    }
+
+    // 3. GÜNCELLEME (POST - AJAX)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(WebSitePassword password)
+    {
+        if (ModelState.IsValid)
+        {
+            password.updated_at = DateTime.Now;
+
+            _context.Update(password);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true, message = "Şifre güncellendi!" });
+        }
+        return Json(new { success = false, message = "Güncelleme başarısız!" });
+    }
+
+    // 4. SİLME (POST - AJAX)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var password = await _context.WebSitePasswords.FindAsync(id);
+        if (password != null)
+        {
+            _context.WebSitePasswords.Remove(password);
+            await _context.SaveChangesAsync();
+            return Json(new { success = true, message = "Şifre silindi." });
+        }
+        return Json(new { success = false, message = "Kayıt bulunamadı." });
+    }
+}
